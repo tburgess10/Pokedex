cfbd767d44912d94f51875d04841f98a9238726a
tburgess10 <tbburgess10@gmail.com>
2026-05-27 08:10:57 -0400

    First test commit

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('index', { title: 'Home', message: 'Welcome to my Node.js website!' });
+  res.render('index', { title: 'Home', message: 'Burgédex' });
 });
 
 router.get('/about', (req, res) => {
